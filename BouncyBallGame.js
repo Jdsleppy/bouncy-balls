c31320d7a8b2d3482bc0217f0c1affb35c2bee75
Joel Sleppy <jdsleppy@gmail.com>
2016-02-20 20:40:56 -0500

    Finish animateBall method not accounting for ball radius

--- BouncyBallGame.js
+++ BouncyBallGame.js
@@ -1,18 +1,43 @@
 var bouncyBallsCanvas = document.getElementById("bouncy-balls");
-
 var balls = [];
+var frameDuration = 30; /* temporal duration of one frame in milliseconds */
 
-animationHandle = setTimeout(function, 30);
+animationHandle = setTimeout(function, frameDuration);
 
 function animateBalls() {
     for (i = 0; i < balls.length; i++) {
-   	    animateBall(balls[i]);
+        animateBall(balls[i]);
     }
 }
 
 function animateBall(ball) {
-		
+		var deltaX = ball.velocityX * (frameDuration / 1000);
+    var deltaY = ball.velocityY * (frameDuration / 1000);
+    /* x movement and bounce */
+    if ((ball.positionX + deltaX) < 0) {
+        ball.positionX = 0;
+        ball.velocityX *= -1;
+    }
+    else if ((ball.positionX + deltaX) < bouncyBallsCanvas.width) {
+        ball.positionX += deltaX;
+    } else {
+        ball.positionX = bouncyBallsCanvas.width;
+        ball.velocityX *= -1;
+    }
+    /* y movement and bounce */
+    if ((ball.positionY + deltaY) < 0) {
+        ball.positionY = 0;
+        ball.velocityY *= -1;
+    }
+    else if ((ball.positionY + deltaXY) < bouncyBallsCanvas.height) {
+        ball.positionY += deltaY;
+    } else {
+        ball.positionY = bouncyBallsCanvas.height;
+        ball.velocityY *= -1;
+    }
 }
 
 /* Use to stop animation */
 /*clearTimeout(animationHandle);*/
+
+
